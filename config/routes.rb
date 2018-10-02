13f62dc041033f1206aaec0f2c8ccbb5910d5035
Delmar Almeida <delmar@Administrators-MacBook-Pro.local>
2018-10-02 10:49:19 +0100

    added devise and User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	root to: "links#index"
+  devise_for :users
   resources :links
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
